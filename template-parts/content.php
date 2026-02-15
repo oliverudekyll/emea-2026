168de9d93af0ae7c5cf97e22ca3e34a2a84f3d9d
oliverudekyll <oliverudekll@gmail.com>
2026-02-15 21:09:21 +0200

    single article redesign, changes to footer styling on mobile, footer logos bigger

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,20 +10,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" class="article">
-	<header class="article__header">
-		<?php
-			$date = get_the_date('d.m.Y');
-			?>
-			<time class="article__date" datetime="<?php echo $date ?>"><?php echo $date ?></time>
-			<?php 
-			the_title( '<h1 class="article__title">', '</h1>' );
-		 ?>
-	</header>
-
-	<div class="article__thumbnail">
-		<?php the_post_thumbnail('large'); ?>
+	<div class="article__header-container">
+		<header class="article__header">
+			<?php
+				$date = get_the_date('d.m.Y');
+				?>
+				<time class="article__date" datetime="<?php echo $date ?>"><?php echo $date ?></time>
+				<?php 
+				the_title( '<h1 class="article__title">', '</h1>' );
+			 ?>
+		</header>
+		
+		<div class="article__thumbnail">
+			<?php the_post_thumbnail('large'); ?>
+		</div>
+		
 	</div>
-
 	<div class="article__content">
 		<?php
 		the_content();
